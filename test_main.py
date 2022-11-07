951419dec32ff8d084df634b932a0e55a748db8a
wafaboussada <wafaboussada@gmail.com>
2022-11-07 23:50:36 +0100

    update

--- test_main.py
+++ test_main.py
@@ -23,6 +23,7 @@ def client():
 
 
 def test_health(client):
+    assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
@@ -38,3 +39,4 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    
